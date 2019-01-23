c6acc6299c66eb41ccbc36c19cc8fe6b30790790
HWFBL <russinho00@gmail.com>
2019-01-23 20:54:12 +0100

    zestaw 12 update

--- zestaw12/12_2.py
+++ zestaw12/12_2.py
@@ -3,25 +3,29 @@ import unittest
 L = [3,4,5,6,8,9,12,15,16]
 
 
-def binary_search_recursive(L, left, right=0, y=None):
-    if y is None:
-        y = len(L) - 1
-    if right > y:
-        return False
+def binary_search_recursive(L, left, right, y):
 
-    mid = (right + y) // 2
-    if left == L[mid]:
+    if right < left:
+        raise ValueError
+    if left == right:
+        if L[left] == y:
+            return left
+        else:
+            return None
+
+    mid = int((left + right) / 2)
+    value = L[mid]
+    if value == y:
         return mid
-    if left < L[mid]:
-        return binary_search_recursive(L, left, right, mid-1)
-    # left > L[mid]
-    return binary_search_recursive(L, left, mid+1, y)
-
+    elif value > y:
+        return binary_search_recursive(L, left, mid - 1, y)
+    else:
+        return binary_search_recursive(L, mid + 1, right, y)
 
 
 class TestPoint(unittest.TestCase):
     def testBinarySearch(self):
-        self.assertEqual(binary_search_recursive(L, 12), 6)
+        self.assertEqual(binary_search_recursive(L, 0, len(L) - 1, 9), 5)
 
 if __name__ == "__main__":
     unittest.main()
--- zestaw12/12_3.py
+++ zestaw12/12_3.py
@@ -4,19 +4,23 @@ import unittest
 
 L = [3,4,5,6,8,9,12,15,16]
 
-def quicksort(L): 
-    if len(L) <= 1:
-        return L
-    else:
-        return quicksort([x for x in L[1:] if x < L[0]]) + \
-               [L[0]] + \
-               quicksort([x for x in L[1:] if x >= L[0]])
-    
-    
+def zamiana(L, left, right):
+    tmp = L[left]
+    L[left] = L[right]
+    L[right] = tmp
+
+def sort(L, left, right):
+    for i in range (left + 1,right + 1):
+        for j in range(left + 1, right + 1):
+            if(L[j] <= L[j-1]):
+                zamiana(L, j, j-1)
+    return L
+
+
 def mediana_sort(L, left, right):
     if len(L) == 0:
         raise ValueError("Lista jest pusta!")
-    L = quicksort(L)
+    L = sort(L, left, right)
     mid = (left+right)/2
     if mid.is_integer():
         return L[int(mid)]
@@ -24,7 +28,7 @@ def mediana_sort(L, left, right):
         x = L[int(math.floor(mid))]
         y = L[int(math.ceil(mid))]
         return (x+y)/2
-
+    
 
 class TestPoint(unittest.TestCase):
     def testBinarySearch(self):
